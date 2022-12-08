3f86d279d02dd7da4bc0bc10404d4320d73db0c8
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-12-08 21:08:27 +0100

    Default thumbnail

--- src/Components/Place/List/thumbnail.js
+++ src/Components/Place/List/thumbnail.js
@@ -4,12 +4,12 @@ function Thumbnail({ place }) {
 
     console.log("thumbnail")
     console.log(place);
-
+    const defaultThumbnail = "/thumbnail/thumbnail.png";
     return (
         <div className='thumbnailContainer' >
             <div className='thumbnail crop'>
-                <img src={place.Thumbnail}></img>
-            </div><br/>
+                <img src={place.Thumbnail ?? defaultThumbnail}></img>
+            </div><br />
             <Link to={"Item\\" + place.id}>{place.Name}</Link>
         </div>)
 }
