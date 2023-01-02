056b94361796756dbd3dd62de060f75c0933f7e3
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-01-02 20:49:02 +0100

    group by last visit

--- src/Components/Place/List/grid.js
+++ src/Components/Place/List/grid.js
@@ -1,66 +1,19 @@
 
+import GroupByYearView from './groupByYearView.js';
+import GroupByLastVisit from './groupByLastVisit.js';
 import Thumbnail from './thumbnail.js'
-function Grid({placeList,grouping }) {
+function Grid({ placeList, grouping }) {
 
-    const getMinimumVisit = (visits) => {
-        let maxVisit = '';
-        visits.forEach(element => {
 
-            console.log("Visita");
-            console.log(element);
-            if (element.Date > maxVisit) {
-                maxVisit = element.Date;
-            }
-        });
-        return maxVisit;
-    }
-
-    const sorting = (a, b) => {
-        if (a.Visits == undefined) {
-            console.log("undefined");
-            return 1;
-        }
-        else {
-            if (b.Visits == undefined) {
-                return -1;
-            }
-            else {
-                debugger;
-                let aMinVisitDate = getMinimumVisit(a.Visits);
-                let bMinVisitDate = getMinimumVisit(b.Visits);
-                if (aMinVisitDate < bMinVisitDate) {
-                    return 1;
-                }
-                else {
-                    return -1;
-                }
-            }
-        }
-    }
 
     if (grouping == 'groupByYear') {
         return (
-            <div>groupByYearView</div>
+            <GroupByYearView></GroupByYearView>
         )
     }
     else {
-        //     return (
-        //         <div>grid
-        //             {
-        //                 {
-        //                 < GroupByYearView ></GroupByYearView>
-        //             }
-        //             else
-        // {
         return (
-            <div>
-                {placeList && placeList.sort(sorting).map(place => {
-                    return (
-                        <Thumbnail place={place} />
-                    )
-                })
-                }
-            </div >
+            <GroupByLastVisit placeList={placeList} ></GroupByLastVisit>
         )
     }
 }
---
+++ src/Components/Place/List/groupByLastVisit.js
@@ -0,0 +1,52 @@
+import Thumbnail from './thumbnail.js'
+
+function groupByLastVisit({ placeList }) {
+    const getMinimumVisit = (visits) => {
+        let maxVisit = '';
+        visits.forEach(element => {
+
+            console.log("Visita");
+            console.log(element);
+            if (element.Date > maxVisit) {
+                maxVisit = element.Date;
+            }
+        });
+        return maxVisit;
+    }
+
+    const sorting = (a, b) => {
+        if (a.Visits == undefined) {
+            console.log("undefined");
+            return 1;
+        }
+        else {
+            if (b.Visits == undefined) {
+                return -1;
+            }
+            else {
+                debugger;
+                let aMinVisitDate = getMinimumVisit(a.Visits);
+                let bMinVisitDate = getMinimumVisit(b.Visits);
+                if (aMinVisitDate < bMinVisitDate) {
+                    return 1;
+                }
+                else {
+                    return -1;
+                }
+            }
+        }
+    }
+
+    return (
+        <div>
+            {placeList && placeList.sort(sorting).map(place => {
+                return (
+                    <Thumbnail place={place} />
+                )
+            })
+            }
+        </div >
+    )
+}
+
+export default groupByLastVisit;
